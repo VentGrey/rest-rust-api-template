8850b1899e9036064b5907a29e47f7e29b405f52
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-29 00:43:55 -0600

    Add static files

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ extern crate rocket;
 mod db;
 mod models;
 mod schema;
+mod static_files;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -23,5 +24,8 @@ fn rocket() -> _ {
     dotenv().ok();
     let db_url: String = env::var("DATABASE_URL").expect("set DATABASE_URL");
     let pool = db::init_pool(db_url);
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount(
+        "/",
+        routes![crate::static_files::all, crate::static_files::index],
+    )
 }
