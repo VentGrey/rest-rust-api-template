ddba635af39b25155bc34684d5f6af7f664cd8a1
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-29 23:25:53 -0600

    Set pool var

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ fn rocket() -> _ {
     let db_url: String = env::var("DATABASE_URL").expect("set DATABASE_URL");
     let pool = db::init_pool(db_url);
     rocket::build()
+        .manage(pool)
         .mount(
             "/api/v1/",
             routes![
